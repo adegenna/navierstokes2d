9380fa690282bd07c04cf0fdac75f886c1512639
Anthony DeGennaro <adegennaro@bnl.gov>
2019-03-19 22:23:52 -0400

    Small stuff

--- output/plot.py
+++ output/plot.py
@@ -2,7 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 t0 = 0
-tf = 3000
+tf = 5000
+nx = 256
+ny = 256
+
+x = np.arange(nx)
+y = np.arange(ny)
+xx,yy = np.meshgrid(x,y)
 
 # w0 = np.genfromtxt('out_' + str(t0) + '.csv' , delimiter=',')
 # wf = np.genfromtxt('out_' + str(tf) + '.csv' , delimiter=',')
@@ -13,15 +19,15 @@ tf = 3000
 
 # plt.show()
 
-tstep = 50
-fig   = plt.figure(10)
+tstep = 100
+fig   = plt.figure(10,figsize=(8,8))
 ax    = fig.gca()
 plt.ion()
 for i in range(int(tf/tstep)):
     print(i*tstep)
     w = np.genfromtxt('out_' + str(int(i*tstep)) + '.csv' , delimiter=',')
     ax.cla()
-    ax.imshow(w,vmin=-1,vmax=1);
+    ax.contourf(xx,yy,w,30,vmin=-.7,vmax=.7);
     plt.pause(0.01)
 
 plt.ioff()
